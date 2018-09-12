1dd637567b91e830e449877be8d5435ad04cc41b
NHRD <naohisa.harada@gmail.com>
2018-09-12 22:17:17 +0900

    Deque first commit.

--- algorithm_week2/deliverables/Deque.java
+++ algorithm_week2/deliverables/Deque.java
@@ -1,66 +1,121 @@
+import java.util.Iterator;
+Import java.util.NoSuchElementException;
+
 Public class Deque<Item> implements Iterable<Item> {
 
-    private Item[] s;
-    private int N = 0;
+    private Node first;
+    private Node last;
+    private int n;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node previous;
+    }
 
     public Deque() {
         //construct an empty deque
-        s = (Item[]) new Object[1];
+        first = null;
+        last = null;
+        n = 0;
     }
 
     public boolean isEmpty() {
         //is the deque empty?
-        return (N ==0);
+        return first == null;
     }
 
     public int size() {
         //return the number of items on the deque.
-        return s.length;
+        return n;
     }
 
     public void addFirst(Item item) {
         //add the item to the front
+        nullcheck(item);
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        if (isEmpty()) {
+            first = last;
+        } else {
+        first.next = oldfirst;
+        oldfirst.previous = first;
+        }
+        n++;
     }
 
     public void addLast(Item item) {
         //add the item to the end
+        nullcheck(item);
+        Node oldlast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+            last.previous = oldlast;
+        }
+        n++;
     }
 
     public Item removeFirst() {
         //remove and return the item from the first
+        removeEmptycheck();
+        Item item = first.item;
+        first = first.next;
+        n--;
+        return item;
     }
 
     public Item removeLast() {
         //remove and return the item from he end
+        removeEmptycheck();
+        Item item = last.item;
+        last = last.previous;
+        n--;
+        return item;
     }
 
     public Iterator<Item> iterator() {
         //return an iterator over items in order from front to end
+        return new ListIterator();
     }
 
-    public static void main(String[] args) {
-        //unit testing(optional)
+    private class ListIterator implements Iterator<Item> {
+        private Node current = first;
+        
+        public boolean hasNext() {
+            return current != null;
+        }
+        
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException("remove() not supported.";)
+        }
+
+        public Item next() {
+            if (current.next == null) {
+                throw new java.lang.NoSuchElementException("No next item exists.");
+            }
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
     }
 
-    private void boundarycheck(String i) {
-        if (i == null) {
+    private void nullcheck(Item item) {
+        //client calls either addFirst() or addLast() with a null argument. 
+        if (item == null) {
             throw new java.lang.IllegalArgumentException("Null is prohibited.");
         }
     }
     
-    private void removeEmpty(){
+    private void removeEmptycheck(){
+        //Client calls either removeFirst() or removeLast when the deque is empty. 
         if (isEmpty()) {
             throw new java.util.NoSuchEementException("Can not remove. Queue is empty.");
         }
-    }
-
-    private void nextcheck(int N) {
-        if (s[N] == null) {
-            throw new java.util.NoSuchEementException("No next item.");
-        }
-    }
-
-    private void remove() { 
-        throw new java.lang.UnsupportedOperationException("Not supported.");
     }
 }
