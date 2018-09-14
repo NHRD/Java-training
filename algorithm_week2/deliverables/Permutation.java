2884389d44c5c6e411cd075329a12e6975da519d
NHRD <naohisa.harada@gmail.com>
2018-09-14 17:18:27 +0900

    52pt

--- algorithm_week2/deliverables/Permutation.java
+++ algorithm_week2/deliverables/Permutation.java
@@ -1,17 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Permutation {
 
     public static void main(String[] args) {
-
-        int nu = Integer.parseInt(args[0]);
-        String[] items = StdIn.readStrings();
-        RandomizedQueue result = new RandomizedQueue();
-        for (int i = 0; i < items.length; i++) {
-            result = result.enqueue(items[i]);
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+        while(!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
         }
-        int j = 0;
-        while (j < nu) {
-            System.out.println(result.dequeue());
-            j++;
+        for (int i = 0; i < k; i++) {
+            StdOut.println(rq.dequeue());
         }
     }
+
 }
--- algorithm_week2/deliverables/RandomizedQueue.java
+++ algorithm_week2/deliverables/RandomizedQueue.java
@@ -1,35 +1,35 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
-    private int n = 0;//size of queue.
+    private int n = 0; //size of queue.
     private Item[] s;
 
 
     public RandomizedQueue() {                
-    // construct an empty randomized queue
-    int defaultsiz = 0;
-    s = (Item[]) new Object[defaultsize];
+        // construct an empty randomized queue
+    s = (Item[]) new Object[1];
     }
     
     public boolean isEmpty() {            
-    // is the randomized queue empty?
+        // is the randomized queue empty?
     return n == 0;
     }
     
     public int size() {                     
-    // return the number of items on the randomized queue
+        // return the number of items on the randomized queue
     return n;
     }
     
     public void enqueue(Item item) {
-        nullcheck();
+        nullcheck(item);
         if (n == s.length) resize(2 * s.length);
         s[n++] = item;
     }
 
-    // remove and return a random item
+        // remove and return a random item
     public Item dequeue() {
         Emptycheck();   
         int index = StdRandom.uniform(n);
@@ -46,16 +46,16 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         int index = StdRandom.uniform(n);
         return s[index];
     }                 
-    // return a random item (but do not remove it)
+        // return a random item (but do not remove it)
     
     public Iterator<Item> iterator() {       
-    // return an independent iterator over items in random order
+        // return an independent iterator over items in random order
         return new ListIterator();
     }
 
     private class ListIterator implements Iterator<Item> {
         private int i = 0;
-        private Item[] = s;
+        private Item[] s;
         private int N;
         
         public boolean hasNext() {
@@ -88,9 +88,9 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         }
     }
 
-    public void resize(int capacity) {
-        Item[] copy = (Item[]) new String[capacity];
-        for (int i = 0; i < N; i++) {
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
             copy[i] = s[i];
         s = copy;
         }
