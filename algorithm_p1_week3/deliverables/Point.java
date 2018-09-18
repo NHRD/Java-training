07efda560bc8866952d6c1a19172ad7fdfda5170
NHRD <naohisa.harada@gmail.com>
2018-09-18 14:25:24 +0900

    a

--- algorithm_p1_week3/deliverables/Point.java
+++ algorithm_p1_week3/deliverables/Point.java
@@ -1,15 +1,34 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
 public class Point implements Comparable<Point> {
 
     public Point(int x, int y) {
-
+        this.x = x;
+        this.y = y;
     }
 
     public void draw() {
-
+        StdDraw.point(x, y);
     }
 
     public void drawTo(Point that) {
+        StdDraw.line(this.x, this.y that.x that.y);
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }   
+
+    public int compareTo(Point that) {
+        return ;
+    }
+
+    public double slopeTo(Point that) {
 
     }
-    
+
+    public Comparator<Point> slopeOrder() {
+
+    }
 }
