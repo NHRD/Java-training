f0ad560f558add9453b0e5b0d776680f3586dc21
NHRD <naohisa.harada@gmail.com>
2018-09-24 20:12:41 +0900

    a

--- algorithm_p1_week3/deliverables/BruteCollinearPoints.java
+++ algorithm_p1_week3/deliverables/BruteCollinearPoints.java
@@ -1,10 +1,22 @@
 public class BruteCollinearPoints {
-    public BruteCollinearPoints(Point[] points) {
 
+    private int segNum = 0;
+
+    public BruteCollinearPoints(Point[] points) {
+        int n = points.length;
+        for (int p = 0; p < n; p++) {
+            for (int q = 0; q < n; q++) {
+                for (int r = 0; r < n; r++) {
+                    for (int s = 0; s < n; s++) {
+                       
+                    }
+                }
+            }
+        }
     }
 
     public int numberOfSegments() {
-        return;
+        return segNum;
     }   
 
     public LineSegment[] segments() {
