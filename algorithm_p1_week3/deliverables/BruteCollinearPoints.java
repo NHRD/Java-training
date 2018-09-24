efec92e03cff07a3a1c34512d23f3a96d09609bd
NHRD <naohisa.harada@gmail.com>
2018-09-24 20:27:18 +0900

    a

--- algorithm_p1_week3/deliverables/BruteCollinearPoints.java
+++ algorithm_p1_week3/deliverables/BruteCollinearPoints.java
@@ -3,12 +3,22 @@ public class BruteCollinearPoints {
     private int segNum = 0;
 
     public BruteCollinearPoints(Point[] points) {
+        Point slope = new Point();
         int n = points.length;
-        for (int p = 0; p < n; p++) {
-            for (int q = 0; q < n; q++) {
-                for (int r = 0; r < n; r++) {
-                    for (int s = 0; s < n; s++) {
-                       
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                       if (i == j || i == k || i == l || j == k || j == l || k == l) {
+                           continue;
+                       }
+                       Point p = points[i];
+                       Point q = points[j];
+                       Point r = points[k];
+                       Point s = points[l];
+                       if (p.slopeTo(q) == p.slopeTo(r) && p.slopeTo(r) == p.slopeTo(s)) {
+                           segNum++;
+                       }
                     }
                 }
             }
