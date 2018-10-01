648ee3571f5642ce1b31fd78cb16837debd6b3f9
NHRD <naohisa.harada@gmail.com>
2018-10-01 21:22:28 +0900

    selection sort added.

---
+++ algorithm_p1_week2/selection_sort.java
@@ -0,0 +1,29 @@
+public class Selection {
+
+    /* 別途作成したComparableを含むデータ型の名称が
+    　 下記のComparableに入る。例えば日付を比べる場合は
+       public static void sort(Date[] a) */
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                exch(a, i, min);
+                }
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}
