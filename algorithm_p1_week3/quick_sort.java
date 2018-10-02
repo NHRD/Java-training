47a9aacd6de5dfbefb74080797b7f2bde358588c
NHRD <naohisa.harada@gmail.com>
2018-10-02 21:57:00 +0900

    a

---
+++ algorithm_p1_week3/quick_sort.java
@@ -0,0 +1,41 @@
+public class Quick {
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        int i = lo;
+        int j = hi + 1;
+        
+        while (true) {
+            while (less(a[++i], a[lo])) {
+                if (i ==hi) {
+                    break;
+                }
+            }
+            while (less(a[lo], a[--j])) {
+                if (j == lo) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        ech(a, lo, j);
+        return j;
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) {
+            return;
+        }
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+}
